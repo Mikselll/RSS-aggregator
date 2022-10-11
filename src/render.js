1eb2d096680bfdebf58c7d464b01792e6861e099
Колосов Станислав <kolosov.stas01@mail.ru>
2022-10-11 23:30:03 +1000

    update

--- src/render.js
+++ src/render.js
@@ -1,28 +1,26 @@
 const renderError = (elements, error, prevError) => {
-    if (prevError === '' && error === '') {
-      return;
-    }
+  if (prevError === '' && error === '') {
+    return;
+  }
 
-    if (prevError !== '' && error === '') {
-      elements.field.classList.remove('is-invalid');
-      return;
-    }
+  if (prevError !== '' && error === '') {
+    elements.field.classList.remove('is-invalid');
+    return;
+  }
 
-    if (prevError !== '' && error !== '') {
-      elements.feedback.textContent = error;
-      return;
-    }
-
-    elements.field.classList.add('is-invalid');
+  if (prevError !== '' && error !== '') {
     elements.feedback.textContent = error;
-}
+    return;
+  }
 
-
+  elements.field.classList.add('is-invalid');
+  elements.feedback.textContent = error;
+};
 
 const render = (elements) => (path, value, prevValue) => {
   if (path === 'form.error') {
     renderError(elements, value, prevValue);
   }
-}
+};
 
 export default render;
--- src/valid.js
+++ src/valid.js
@@ -1,22 +1,23 @@
 import onChange from 'on-change';
+import keyBy from 'lodash/keyBy.js';
 import * as yup from 'yup';
 import render from './render.js';
-import keyBy from 'lodash/keyBy.js';
 
 const added = [];
 
 const schema = yup.object().shape({
-    website: yup.string()
-    .matches(/^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm, 'Ссылка должна быть валидным URL')
+  website: yup
+    .string()
+    .url()
     .notOneOf(added),
 });
 
 const validate = (field) => {
   try {
     schema.validateSync(field, { abortEarly: false });
-    return {};
+    return '';
   } catch (e) {
-    return keyBy(e.inner, 'path')
+    return e.message;
   }
 };
 
@@ -26,25 +27,27 @@ const valid = () => {
     field: document.querySelector('#url-input'),
     submit: document.querySelector('.btn-lg'),
     feedback: document.querySelector('.feedback'),
-  }
+  };
 
   const state = onChange({
     form: {
       valid: true,
       field: {
-        website: '', 
+        website: '',
       },
       error: '',
-      }
-  }, render(elements))
+    },
+  }, render(elements));
 
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
-    state.field.website = elements.field.textContent;
-    const error = validate(state.form.field).website.message;
+
+    state.form.field.website = elements.field.textContent;
+    const error = validate(state.form.field);
+    console.log(error);
     state.form.error = error;
-    state.form.valid = state.form.error = '' ? true : false;
-  })
-}
+    state.form.valid = state.form.error === '';
+  });
+};
 
 export default valid;
