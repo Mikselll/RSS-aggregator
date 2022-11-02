fa656e76e9139f88edf0eda4c1cb49bf143ffc9b
Колосов Станислав <kolosov.stas01@mail.ru>
2022-11-02 21:59:17 +1000

    add locales

---
+++ src/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    feedbacks: {
+      notOneOf: 'RSS уже существует',
+      required: 'Не должно быть пустым',
+      invalidURL: 'Ссылка должна быть валидным URL',
+      networkError: 'Ошибка сети',
+      parsingError: 'Ресурс не содержит валидный RSS',
+      unknown: 'Неизвестная ошибка',
+      validRSS: 'RSS успешно загружен',
+    },
+    btn: 'Просмотр',
+    posts: 'Посты',
+    feeds: 'Фиды',
+  },
+};
