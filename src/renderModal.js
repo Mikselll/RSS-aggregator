f0719b2251d33970ab791a0858831fc707c45871
Колосов Станислав <kolosov.stas01@mail.ru>
2022-11-17 21:11:05 +1000

    add render modal

---
+++ src/renderModal.js
@@ -0,0 +1,12 @@
+const renderModal = (state, UIstate, elements) => {
+  const link = document.querySelector(`[data-id="${UIstate.currentPostId}"]`);
+  link.removeAttribute('class');
+  link.classList.add('fw-normal', 'link-secondary');
+
+  const currentPost = state.posts.flat().filter((post) => post.id === UIstate.currentPostId);
+  elements.modalTitle.textContent = currentPost.title;
+  elements.modalBody.textContent = currentPost.description;
+  elements.modalLink.setAttribute('href', currentPost.link);
+};
+
+export default renderModal;
