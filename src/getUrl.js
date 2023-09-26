f1e8a3f2e539cfeefcb753010cd9ccb728513941
Колосов Станислав <kolosov.stas01@mail.ru>
2023-09-26 22:02:24 +1000

    fix1

--- src/getUrl.js
+++ src/getUrl.js
@@ -1,4 +1,5 @@
 const getUrl = (url) => {
+  const a = 1;
   const proxyURL = new URL('https://allorigins.hexlet.app/get');
   proxyURL.searchParams.set('disableCache', true);
   proxyURL.searchParams.set('url', url);
